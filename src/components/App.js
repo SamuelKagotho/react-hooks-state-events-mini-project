8789ba0b8fbfad1ba4d59a8bf7c0fda90a565e05
samuelkagotho <samuelkagotho@gmail.com>
2024-11-27 17:11:00 +0430

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Filter tasks based on selected category
+  const visibleTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={(newTask) => setTasks([...tasks, newTask])}
+      />
+      <TaskList tasks={visibleTasks} setTasks={setTasks} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, setTasks }) {
+  const handleDelete = (taskToDelete) => {
+    setTasks(tasks.filter((task) => task.text !== taskToDelete.text));
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={handleDelete} />
+      ))}
     </div>
   );
 }
